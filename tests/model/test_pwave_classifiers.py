d594747d7f5027b6994d98eaa17ed59d6dcb40de
Kjell Le <lkjell@gmail.com>
2017-12-15 18:09:49 +0100

    Fix test path to file dir to be able to load classifier data

--- tests/model/test_pwave_classifiers.py
+++ tests/model/test_pwave_classifiers.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
+import os
 import numpy as np
 from construe.knowledge.abstraction_patterns.segmentation.pwave import _CLASSIFIERS as classifier
+
+path = os.path.dirname(__file__)
 
 
 class TestClassifier(TestCase):
@@ -8,7 +11,7 @@ class TestClassifier(TestCase):
         limb = classifier[0]
         prec = classifier[1]
 
-        X_test = np.loadtxt("pw_samples.csv", delimiter=",", skiprows=1)
+        X_test = np.loadtxt("%s/pw_samples.csv" % path, delimiter=",", skiprows=1)
         X_test, Y_test = X_test[:, 0:8], X_test[:, 8:]
 
         d1 = limb.decision_function(X_test)
